19c5d8844db8db3e32ca9ca8133b6bce590e931a
Alexey Shutov <alexeyshutov1@icloud.com>
2025-03-08 03:33:30 +0300

    added basic tests

---
+++ tests/station_list_test.rs
@@ -0,0 +1,9 @@
+use tokio;
+use yandex_rasp_api;
+
+#[tokio::test]
+async fn test() {
+    let api_key = std::env::var("API_KEY").unwrap();
+    let client = yandex_rasp_api::YaRaspClient::new(&api_key);
+    assert!(client.stations_list().send().await.is_ok())
+}
